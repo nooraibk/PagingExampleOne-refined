88558205748beb587575732362ec3297dd3f9c7a
nooraibk <subject-works.culvert@aleeas.com>
2024-03-29 09:38:18 +0500

    fourth commit

---
+++ app/src/main/java/com/example/pagingexampleone/data/network/CatPagingSource.kt
@@ -0,0 +1,34 @@
+package com.example.pagingexampleone.data.network
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.pagingexampleone.data.network.dtos.CatDto
+import java.io.IOException
+
+const val STARTING_PAGE_INDEX = 1
+class CatPagingSource(private val catApi: CatApi): PagingSource<Int, CatDto>() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CatDto> {
+        val page = params.key?: STARTING_PAGE_INDEX
+        return try {
+            val response = catApi.getCatImages(page = page, size = params.loadSize)
+            LoadResult.Page(
+                data = response,
+                prevKey = if (page == STARTING_PAGE_INDEX) null else page - 1,
+                nextKey = if (response.isEmpty()) null else page + 1
+            )
+        }catch (e : IOException){
+            LoadResult.Error(e)
+        }catch (e : HttpException){
+            LoadResult.Error(e)
+        }
+
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, CatDto>): Int? {
+
+    }
+}
